f2ea361f844f00588de8969dd61779cf5766e4c8
unknown <miemail@dominio.com>
2021-10-31 11:18:34 -0500

    DataTable_v2

--- php/consulta.php
+++ php/consulta.php
@@ -4,25 +4,25 @@
     $sql = "SELECT * FROM persona"; //codigo SQL
 
     $consulta = mysqli_query($respuesta,$sql) or die(mysqli_error());
-    // $tabla= ""; //otra forma de crear el jason
+     //$tabla= ""; //otra forma de crear el jason
     $retorno_array = array();
 
     while($fila = mysqli_fetch_array($consulta)){
-        $row_array['id']             = utf8_encode($fila['id']);
-        $row_array['nombre']         = utf8_encode($fila['nombre']);
-        $row_array['documento']      = utf8_encode($fila['documento']);
-        $row_array['tel']            = utf8_encode($fila['tel_1']);
-        $row_array['direccion']      = utf8_encode($fila['direccion']);
+        $row_array['id']             = $fila['id'];
+        $row_array['nombre']         = $fila['nombre'];
+        $row_array['documento']      = $fila['documento'];
+        $row_array['tel']            = $fila['tel_1'];
+        $row_array['direccion']      = $fila['direccion'];
 
         array_push($retorno_array, $row_array);
 
-        // $tabla.='{ //otra forma de crear el jason
-        //     "id":"'.$fila['id'].'", //otra forma de crear el jason
-        //     "nombre":"'.$fila['nombre'].'", //otra forma de crear el jason
-        //     "documento":"'.$fila['documento'].'", //otra forma de crear el jason
-        //     "tel":"'.$fila['tel_1'].'", //otra forma de crear el jason
-        //     "direccion":"'.$fila['direccion'].'" //otra forma de crear el jason
-        // },'; //otra forma de crear el jason
+        // $tabla.='{ 
+        //     "id":"'.$fila['id'].'", 
+        //     "nombre":"'.$fila['nombre'].'", 
+        //     "documento":"'.$fila['documento'].'", 
+        //     "tel":"'.$fila['tel_1'].'", 
+        //     "direccion":"'.$fila['direccion'].'" 
+        // },'; 
     }
 
         print ('{"data":'.json_encode($retorno_array).'}');
